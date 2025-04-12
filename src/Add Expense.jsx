2db9e55632bfb8384d5a44e07ac643853e0c24b3
Mercy <maritimmercy8@gmail.com>
2025-04-12 16:52:06 +0300

    added table

---
+++ src/Add Expense.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function AddExpense() {
+  return (
+    <form action="/action_page.php">
+      <label for="fname">First name:</label>
+      <br />
+      <input type="text" id="fname" name="fname" value="John" />
+      <br />
+      <label for="lname">Last name:</label>
+      <br />
+      <input type="text" id="lname" name="lname" value="Doe" />
+      <br />
+      <br />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Table from './components/Table'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Table/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +30,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
     </>
+
   )
 }
 
